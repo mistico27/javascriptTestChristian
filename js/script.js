8b2da5324253e3ab4588c812bcf2633aebc8507f
chris_beltran <cristianb72@hotmail.com>
2023-04-20 22:03:32 -0600

    primerEjercicio

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let r =10;
+const pi = 3.1416;
+let Area = pi *r *r;
+
+console.log("hola soy  el primer programa");
+console.log("El el radio del circulo es " + Area);
+
+console.log(" ");
+console.log("Este es el segundo programa");
+if(Area>10){
+    console.log("El Area del circulo es mayor a 10 unidades cuadradas");
+}else{
+    console.log("El area es menor o es igual a 10 unidades cuadradas");
+}
+
+console.log(" ");
+console.log("Este es el tercer programa");
+let somos = "somos";
+let koders= "koders"
+console.log(somos+" "+koders);
