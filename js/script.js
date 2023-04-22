fcc12f48b3b4026df01721fbb70486fa38fb472d
chris_beltran <cristianb72@hotmail.com>
2023-04-21 22:00:41 -0600

    voladorjr

--- js/script.js
+++ js/script.js
@@ -18,3 +18,62 @@ console.log("Este es el tercer programa");
 let somos = "somos";
 let koders= "koders"
 console.log(somos+" "+koders);
+
+
+console.log("");
+console.log("Este es el problema de los poligonos");
+function getAreaForPolygon(lados,perimetro, apotema){
+    if(lados<5){
+        console.log("No funciona esta formula, intente con otra... por favor");
+        return null;
+    }else{
+        let result= (perimetro*apotema)/2;
+        return result;
+    }
+    
+
+}
+
+
+let poligono = getAreaForPolygon(5,10,5);
+console.log("Area es igual a " +poligono + " Unidades cuadradas");
+
+
+
+console.log("")
+
+console.log("Esta seccion es para las funcion flecha");
+console.log("Programa 1");
+
+var biggerOrSmaller = function (num) {
+    var res = true;
+    if(num% 2 === 0 ){
+        console.log("Este numero es par");
+        
+    }else{
+        console.log("Este numero  NO es par");
+        res= false;
+    }
+    return res;
+  };
+  console.log( biggerOrSmaller(39) ); 
+
+
+  console.log("");
+  console.log("Programa 2");
+  
+  var MAYOR = function (num) {
+      if(num >=18 ){
+          console.log("es mayor de edad");
+      }else{
+          console.log("tio no puede tomar cerveza aun");
+      }
+    };
+    console.log( MAYOR(39) ); 
+
+
+
+
+
+
+
