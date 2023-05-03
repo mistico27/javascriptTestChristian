1a7b28a6ca0cf37b7587f03fe170a38dab93a8f2
chris_beltran <cristianb72@hotmail.com>
2023-05-02 19:44:04 -0600

    login complete

--- js/auth.js
+++ js/auth.js
@@ -1,101 +1,20 @@
-let mentors = [
-    ["Naomi", "López"], 
-    ["Israel", "Salinas"], 
-    ["Charles", "Silva"], 
-    ["Rose", "Ortega"], 
-  ];
-  let koders = [
-    {
-      name: "Verónica Cruz",
-      scores: {
-        html: 10,
-        css: 9.2,
-        js: 9.7,
-        bootstrap: 8,
-      },
-      isGraduated: true,
-    },
-    {
-      name: "Miguel Aceves",
-      scores: {
-        html: 8.7,
-        css: 10,
-        js: 8.3,
-        bootstrap: 9.8,
-      },
-      isGraduated: false,
-    },
-    {
-      name: "Antonio Cano",
-      scores: {
-        html: 9.2,
-        css: 10,
-        js: 10,
-        bootstrap: 6.5,
-      },
-      isGraduated: false,
-    },
-  ];
-  /*
-      1.- Necesito una lista que contenga los nombres completos de los mentores ordenados alfabéticamente
-      2.- Necesito una lista con los nombres y promedios generales de todos los koders
-      3.- Necesito una lista con los nombres de aquellos koders que aún no se han graduado
-      4.- Necesito conocer el promedio grupal de cada materia impartida en el bootcamp
-          html -> 9
-          css -> 10
-      5.- Necesito saber cúal es el koder con el promedio más alto y cúal el que tiene el promedio más bajo
-      */
+class Auth {
+	constructor() {
+        document.querySelector("body").style.display = "none";
+		const auth = localStorage.getItem("auth");
+		this.validateAuth(auth);
+	}
 
-console.log("Primer ejercicio");
+	validateAuth(auth) {
+		if (auth != 1) {
+			window.location.replace("/");
+		} else {
+            document.querySelector("body").style.display = "block";
+		}
+	}
 
-const myMentors= (arreglo) =>{
-    for(let z=0; z<arreglo.length; z++){
-      console.log(arreglo[z][0] +" " + arreglo[z][1]);   
-    }
+	logOut() {
+		localStorage.removeItem("auth");
+		window.location.replace("/");
+	}
 }
-
-//let test= myMentors(mentors);
-
-
-console.log("Programa II");
-
-
-const myGrades =(arreglo) =>{
-    for(let i=0; i<arreglo.length; i++){
-        let sumtotal =arreglo[i].scores.html + arreglo[i].scores.css + arreglo[i].scores.js+arreglo[i].scores.bootstrap;
-        
-        console.log(arreglo[i].name + " Promedio total " + sumtotal/4);      
-    }
-}
-
-//let test= myGrades(koders);
-
-console.log("porgrama III");
-
-const kodersUngraduated =(arreglo) =>{
-    for(let z=0; z<arreglo.length; z++){
-        if(arreglo[z].isGraduated!=true){
-            let segraduo =`${arreglo[z].name}  , no se ha graduado`;
-            console.log(segraduo);
-        }
-        
-        
-    }
-}
-
-
-//let nuevotest = kodersUngraduated(koders);
-
-console.log("programa IV");
-
-
-const groupalGrades =(arreglo) =>{
-    for(let i=0; i<arreglo.length; i++){
-        let a = arreglo[i].scores.html;
-        a =a+a;
-        console.log(a);
-    }
-}
-
-
-let total = groupalGrades(koders);
--- js/script.js
+++ js/script.js
@@ -1,1 +1,123 @@
-console.log("MAria");
+class Login {
+	constructor(form, fields) {
+		this.form = form;
+		this.fields = fields;
+		this.validateonSubmit();
+	}
+
+    validateonSubmit(){
+        let self=this;
+        this.form.addEventListener("submit", (e) => {
+          e.preventDefault();
+          var error = 0;
+          self.fields.forEach((field) => {
+              const input = document.querySelector(`#${field}`);
+              if (self.validateFields(input) == false) {
+                  error++;
+              }
+          });
+          if (error == 0) {
+              localStorage.setItem("auth", 1);
+              this.form.submit();
+          }
+      });
+      }
+
+
+       containsSpecialChars(str) {
+        const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+        return specialChars.test(str);
+      }
+
+
+      containsNumbers(str){
+        const numbers = /[1234567890]/;
+        return numbers.test(str);
+
+      }
+
+    validateFields(field){
+        const data = document.getElementById('username');
+     
+        if(field.value.trim() ==""){
+            this.setStatus(
+                field,
+                `${field.previousElementSibling.innerText} can not be blank`,
+                "error"
+            );
+            return false;
+        }else{
+            if(field.type =="password"){
+                if(field.value.length<8){
+                    this.setStatus(
+                        field,
+                        `${field.previousElementSibling.innerText},  Must be at last 8 characters`,
+                        "error"
+                    );
+                    return false;
+                }else{
+                    this.setStatus(field,null,"success");
+                    return true;
+                }
+            }else if(data!=''){
+                if(this.containsSpecialChars(data.value)== true){
+                    this.setStatus(
+                        field,
+                        `${field.previousElementSibling.innerText},  Special characteres`,
+                        "error"
+                    );
+                    return false;
+                }else if(this.containsNumbers(data.value)== true){
+                    
+                        this.setStatus(
+                            field,
+                            `${field.previousElementSibling.innerText},  contains numbers`,
+                            "error"
+                        );
+                        return false;
+                }
+                
+                else{
+                    this.setStatus(field,null,"success");
+                    return true;
+                }
+
+            }else{
+                this.setStatus(field,null,"success");
+                return true;
+            }
+        }
+    }
+
+    setStatus(field,message,status){
+        const errorMessage = field.parentElement.querySelector(".error-message");
+
+        if(status =="success"){
+            if(errorMessage){
+                errorMessage.innerText="";
+            }
+            field.classList.remove("input-error");
+        }
+
+        if(status=="error"){
+            errorMessage.innerText =message;
+            field.classList.add("input-error");
+        }
+    }
+
+}
+
+const form = document.querySelector('.loginForm');
+if(form){
+  const fields =["username","password"];
+  const validator=new Login(form,fields);
+}
+
+
+  
+function containsSpecialChars(str) {
+    const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    return specialChars.test(str);
+  }
+console.log(containsSpecialChars("kasimiro"));
+console.log(containsSpecialChars("christian"));
