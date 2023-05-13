51b3298ba309d03451ed1d738b4f7120c9dc8579
chris_beltran <cristianb72@hotmail.com>
2023-05-12 21:35:17 -0600

    nueModalII

--- js/script.js
+++ js/script.js
@@ -1,9 +1,28 @@
 const table = document.getElementById('table');
 const modal = document.getElementById('modal');
+const inputs = document.querySelectorAll('input');
+let count=0;
 
 
-table.addEventListener('click',(evento)=>{
-evento.stopPropagation();
-console.log(evento.target.parentElement.parentElement.child);
+window.addEventListener("click",(e)=>{
+    if(e.target.matches(".btn-warning")){
+        let data= e.target.parentElement.parentElement.children;
+        fillData(data);
+        modal.classList.toggle("translate");
+    }
 
+
+    if(e.target.matches(".btn-danger")){
+        modal.classList.toggle("translate");
+        count=0;
+    }
 });
+
+
+const fillData = (data)=>{
+        for(let index of inputs){
+            index.value= data[count].textContent;
+            console.log(index);
+            count++;
+        }
+}
