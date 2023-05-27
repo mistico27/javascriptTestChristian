4dc02a0061f2ede9ff91f85ba74f656e059d8f87
chris_beltran <cristianb72@hotmail.com>
2023-05-27 02:01:53 -0600

    mistico

--- nodejs/js/app.js
+++ nodejs/js/app.js
@@ -1,297 +1,68 @@
-///primero pondremos el endpoint
-const BASE_URL ='https://caballeroszodiaco-bb6c6-default-rtdb.firebaseio.com';
-//crear el objeto de caballeros y el id
-let knightObject={};
-let knoghts=[];
-let knightId =null;
+///arreglo
+const personas=[
+  new Persona('christian','beltran'),
+  new Persona('karla','manzano')
+];
 
-///funcionalidades
-function showAlert(message,className){
-  const div=document.createElement('div');
-  div.className=`alert alert-${className}`;
-  div.appendChild(document.createTextNode(message));
-  const container =document.querySelector('.container');
-  const form = document.querySelector('#zodiac-form');
-  
-  form.parentNode.insertBefore(div,form);
 
-  ///vanish
-  setTimeout(()=>document.querySelector('.alert').remove(),2500)
+function mostrarPersonas(){
+  console.log("mostrar");
+  let texto='';
+  ///for of de cada persona
+  for(let persona of personas){
+    console.log(persona);
+    texto +=`<li>${persona.nombre}  ${persona.apellido}</li>`;
+  }
 
-}
-//special Characters
-function containsSpecialChars(str) {
-  const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
-  return specialChars.test(str);
-}
-
-function containsNumbers(str){
-  const numbers = /[1234567890]/;
-  return numbers.test(str);
+  ///mostrar elementos del Dom
+  document.getElementById('personas').innerHTML=texto;
 
 }
 
 
-///Save and Edit
-const saveKnight= async (knight,knightId)=>{
-  if(knight.nombre ===''|| knight.signo===''||knight.picture===''||knight.orden===''||knight.tecnica===''){
-    showAlert('please fill the form','danger');  
-    } else if(containsSpecialChars(knight.nombre)||containsSpecialChars(knight.signo)||containsSpecialChars(knight.tecnica)||containsSpecialChars(knight.orden)){
-      showAlert('please Do not set special charcateres','danger');
-    }else if(containsNumbers(knight.nombre)||containsNumbers(knight.signo)||containsNumbers(knight.orden)){
-      showAlert('please Do not set numbers','danger');
-    }else{
+function agregarPersona(){
+  const forma = document.forms['forma'];
+  const nombre = forma['nombre'];
+  const apellido= forma['apellido'];
+  if(nombre.value ==='' || apellido.value === ''){
+    alert("no hay informacion para agregar");
+  }else{
+    const persona = new Persona(nombre.value,apellido.value);
+    personas.push(persona);  
+    mostrarPersonas();
+  }
   
-    if(knightId){
-      let response = await fetch(`${BASE_URL}/${knightId}/.json`,{
-        method:'PUT',
-        body:JSON.stringify(knight),
-      });
-      let data= await response.json();
-      return data;
-    }else{
-      let response =await fetch(`${BASE_URL}/.json`,{
-        method:'POST',
-        body:JSON.stringify(knight),
-      });
-      let data= await response.json();
-      return data;
-    }
-  }
- 
-};
+}
+////reloj 
+const mostrarReloj =() =>{
+  let fecha = new Date();
+  let hr= fomtatoHora(fecha.getHours());
+  let min=fomtatoHora(fecha.getMinutes());
+  let sec =fomtatoHora(fecha.getSeconds());
+  document.getElementById('hora').innerHTML=`${hr}:${min}:${sec}`;
 
-///Mandar llamar el metodo
-document.getElementById('save-knight').addEventListener("click",async(event)=>{
-  event.preventDefault();
-  document.querySelectorAll('#zodiac-form input').forEach((item)=>{
-    knightObject[item.name]=item.value;
-  });
-  let response = await saveKnight(knightObject,knightId);
-  if(response){
-    printAllKnights('caballeros-list');
-  }
-knightId=null;
-cleanForm();
-
-});
-
-const cleanForm =()=>{
-  let inputs=document.querySelectorAll('#zodiac-form input');
-  inputs.forEach(item =>item.value="")
+  const meses = ['Ene','feb',"mar","abril","may","jun","jul","ago","sept","oct","nov","dic"];
+  const dias =["lunes","martes","miercoles","jueves","viernes","sabado","domingo"];
+  let diaSemana = dias[fecha.getDay()];
+  let dia = fecha.getDate();
+  let mes = meses[fecha.getMonth()];
+  let year =fecha.getFullYear()
+  let fechaTexto = `${diaSemana} ${dia} ${mes} ${year}`;
+  document.getElementById('fecha').innerHTML=fechaTexto;
+  document.getElementById('contenedorII').classList.toggle("animar");
 }
 
-///create card
-
-const createKnightCard=(KnightData,knightkey)=>{
-  let {nombre,orden,picture,signo,tecnica}=KnightData;
-  let cardcol=document.createElement("div");
-  cardcol.classList.add("col");
-
-  let cardWrapper = document.createElement("div");
-  cardWrapper.classList.add("knight-card","card","mb-3");
-  
-  let cardRow = document.createElement("div");
-  cardRow.classList.add("row","g-0");
-
-  let imageCol = document.createElement("div");
-  imageCol.classList.add("card-img-top");
-
-  let cardPicture =document.createElement("img");
-  cardPicture.classList.add("card-picture");
-  cardPicture.setAttribute("src",picture);
-
-  let contentCol = document.createElement("div");
-  contentCol.classList.add("col-md-4");
-
-  let cardBody = document.createElement("div");
-  cardBody.classList.add(
-    "card-body",
-    "h-100",
-    "d-flex",
-    "flex-column",
-    "justify-content-between"
-  );
-
-  
-let cardTitle = document.createElement("h5");
-cardTitle.classList.add("card-title");
-let cardTitleText = document.createTextNode(` ${orden}`);
-cardTitle.append(cardTitleText);
 
 
-let cardKnightName =document.createElement("p");
-cardKnightName.classList.add("card-text");
-let knightName = document.createTextNode(` ${nombre}`);
-cardKnightName.append(knightName);
-
-
-let cardKnightsignno =document.createElement("p");
-cardKnightsignno.classList.add("card-textI");
-let knightSigno = document.createTextNode(` ${signo}`);
-cardKnightsignno.append(knightSigno);
-
-
-let cardKnightTecnica =document.createElement("p");
-cardKnightTecnica.classList.add("card-textIII");
-let knightThecnic = document.createTextNode(`${tecnica} `);
-cardKnightTecnica.append(knightThecnic);
-
-let buttonWrapper =document.createElement("div");
-buttonWrapper.classList.add(
-  "d-flex",
-  "justify-content-between",
-  "flex-column",
-  "flex-md-row",
-  "gap-3"
-);
-
-let deleteButton =document.createElement("button");
-deleteButton.classList.add("btn","btn-danger");
-let deleteText =document.createTextNode("borrar");
-deleteButton.append(deleteText);
-deleteButton.addEventListener("click",()=>{
-    deleteKnight(knightkey);
-});
-
-let modifiedButton =document.createElement("button");
-modifiedButton.classList.add("btn","btn-primary");
-let modifiedText =document.createTextNode("Modificar");
-modifiedButton.append(modifiedText);
-modifiedButton.addEventListener("click",()=>{
-modifiedKnight(knightkey);
-
-});
-
-///aqui va el detalle
-let detailButton = document.createElement("button");
-detailButton.classList.add("btn","btn-warning");
-let ViewDetailText =document.createTextNode("Detalle");
-detailButton.append(ViewDetailText);
-
-detailButton.addEventListener("click",()=>{
-  window.location.replace('./detailedView.html');
-  
-  });
-
-
-////////////////////
-buttonWrapper.append(deleteButton,modifiedButton,detailButton);
-cardBody.append(cardTitle,cardKnightName,cardKnightTecnica,buttonWrapper);
-contentCol.append(cardBody);
-imageCol.append(cardPicture);
-cardRow.append(imageCol,contentCol);
-cardWrapper.append(cardRow);
-cardcol.append(cardWrapper);
-return cardcol;
-
-};
-
-
-const getAllKnights =async ()=>{
-  let response= await fetch(`${BASE_URL}/.json`);
-  let data = await response.json();
-  return data;
-};
-
-
-
-const printAllKnights =async (listId)=>{
-   knoghts =await getAllKnights();
-  let listWrapper =document.getElementById(listId);
-  while(listWrapper.firstChild){
-    listWrapper.removeChild(listWrapper.firstChild);
+const fomtatoHora=(hora)=>{
+  if(hora<10){
+    hora = '0'+hora;
   }
-  for (key in knoghts ){
-    let knoghtData = knoghts[key];
-    let card=createKnightCard(knoghtData,key);
-    listWrapper.appendChild(card);
-  }
-
-};
+  return hora;
+}
 
 
-
-
-
-printAllKnights("caballeros-list");
-
-
-const deleteKnight =async (knightkey) =>{
-  let response =await fetch(`${BASE_URL}/${knightkey}/.json`,{
-    method:"DELETE",
-  });
-  let data =await response.json();
-  printAllKnights("caballeros-list");
-
-};
-
-
-const modifiedKnight =async(knightkey)=>{
-  knightId=knightkey;
-  let newResponse = await fetch(`${BASE_URL}/${knightId}/.json`);
-  let newData = await newResponse.json();
-  let listInput =document.querySelectorAll("form input");
-  listInput.forEach(item=>{
-    item.value=newData[item.name];
-  });
-
-};
-///search knight
-const KnightSearchInput =document.querySelector("[data-search]");
-
-
-
-KnightSearchInput.addEventListener("input",(e)=>{
-  const value = e.target.value.toLowerCase();
-  const storeItems =document.getElementById('caballeros-list');
-  const product =document.querySelectorAll(".knight-card");
-  const productName =storeItems.getElementsByTagName("h5");
-
-  for(var i=0;i<productName.length;i++){
-    let match=product[i].getElementsByTagName('h5')[0];
-    if(match){
-     let texvalue= match.textContent || match.innerHTML
-     
-        if(texvalue.toLowerCase().indexOf(value)>-1){
-            product[i].style.display="";
-
-        }else{
-          product[i].style.display="none";
-
-        }
-    }
-  }
-
-
-});
-
-const printSortAllKnights =async (listId)=>{
- 
-  knoghts =await getAllKnights();
-  const newSortArray =Object.values(knoghts).reverse();
-
-  var JsonObject = JSON.parse(JSON.stringify(newSortArray));
-  console.log(JsonObject);
- let listWrapper =document.getElementById(listId);
- while(listWrapper.firstChild){
-   listWrapper.removeChild(listWrapper.firstChild);
- }
- for (key in JsonObject ){
-   let knoghtData = JsonObject[key];
-   let card=createKnightCard(knoghtData,key);
-   listWrapper.appendChild(card);
- }
-
-};
-
-const reverseButton =document.getElementById("find-latest");
-reverseButton.addEventListener('click',(e)=>{
-  e.preventDefault();
-  printSortAllKnights("caballeros-list");
-});
-
-
-
+//solo referencia
+setInterval(mostrarReloj,1000);
 
 
