852cbc0f33fa9fe15a5f1a52ade60c008214f52b
chris_beltran <cristianb72@hotmail.com>
2023-07-14 22:23:35 -0600

    test

--- nodejs/newProject/reactTesting/src/test/button.test.jsx
+++ nodejs/newProject/reactTesting/src/test/button.test.jsx
@@ -1,8 +1,7 @@
+import { afterEach, beforeEach, describe, expect, test } from "vitest";
 import Button from "../components/button";
-import React from "react";
-import { beforeEach, describe, expect, test,afterEach } from "vitest";
 import matchers from "@testing-library/jest-dom/matchers";
-import { cleanup,screen,render} from "@testing-library/react";
+import { cleanup, fireEvent, render, screen } from "@testing-library/react";
 
 expect.extend(matchers);
 describe("button Contender",()=>{
@@ -13,6 +12,5 @@ afterEach(()=>cleanup());
      const button= screen.getByText('Daleclick');
      expect(button).toBeVisible();
     })
-
-
+    
 })
